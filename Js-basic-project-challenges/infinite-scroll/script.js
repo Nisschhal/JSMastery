ecfd8cbee31fd0759533e5a08a138f3da805efe9
Nischal Puri <mrnischalpuri@gmail.com>
2023-02-21 21:55:52 +0545

    infinite scroll completed

---
+++ Js-basic-project-challenges/infinite-scroll/script.js
@@ -0,0 +1,80 @@
+// UNSPLASH API
+
+import { UNSPLASH_API_KEY } from "../../keys.js";
+const imageContainer = document.getElementById("image-container");
+const loaderEl = document.getElementById("loader");
+console.log(imageContainer.children);
+const unSplashAPI = `https://api.unsplash.com/photos/random/?client_id=${UNSPLASH_API_KEY}&count=10
+`;
+
+let photos = [];
+
+const get10Photos = async () => {
+  try {
+    loaderEl.hidden = false;
+    const res = await fetch(unSplashAPI);
+    console.log(res);
+    photos = await res.json();
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+// GET THE PHOTO FROM SPLASH
+await get10Photos();
+
+// SET ELEMENT ATTRIBUTES
+function setAttribute(item, attributes) {
+  for (const key in attributes) {
+    item.setAttribute(key, attributes[key]);
+  }
+}
+
+// DISPLAY TO HTML
+function displayPhotos() {
+  console.log(photos);
+  photos.forEach((photo) => {
+    // anchor <a> tag element
+    const item = document.createElement("a");
+    setAttribute(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    // item.setAttribute("href", photo.links.html);
+    // item.setAttribute("target", "_blank");
+
+    // image <img> tag element
+    const img = document.createElement("img");
+    setAttribute(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // img.setAttribute("src", photo.urls.regular);
+    // img.setAttribute("alt", photo.alt_description);
+    // img.setAttribute("title", photo.alt_description);
+
+    // element append to it parent
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+    loaderEl.hidden = true;
+  });
+}
+
+displayPhotos();
+window.addEventListener("scroll", (e) => {
+  if (
+    window.scrollY + window.innerHeight >=
+    document.body.offsetHeight - 1000
+  ) {
+    get10Photos();
+    displayPhotos();
+  }
+  //   console.log("inner height", window.innerHeight);
+  //   console.log("scrollY", window.scrollY);
+  //   console.log(window.innerHeight + window.scrollY);
+  //   console.log("offset", document.body.offsetHeight);
+});
+// window.addEventListener("click", (e) =>
+//   console.log(e.clientX, document.body.offsetHeight)
+// );
---
+++ keys.js
@@ -0,0 +1,1 @@
+export const UNSPLASH_API_KEY = "eIyLbr1vMgA90j3NW7VECkhcmW-Ou-F01sH1zgVpzy4";
